1502949fe98c8c00379f41fa231f8e64a704b010
Luciano Passalenti <luchontandil3@gmail.com>
2020-06-15 00:54:58 +0100

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -181,6 +181,30 @@ class UserController extends Controller
       imagejpeg($thumb, $filename, 80);
 
  }
+ function imagecreatefromfile( $filename ) {
+     if (!file_exists($filename)) {
+         throw new InvalidArgumentException('File "'.$filename.'" not found.');
+     }
+     switch ( strtolower( pathinfo( $filename, PATHINFO_EXTENSION ))) {
+         case 'jpeg':
+         case 'jpg':
+             return imagecreatefromjpeg($filename);
+         break;
+
+         case 'png':
+             return imagecreatefrompng($filename);
+         break;
+
+         case 'gif':
+             return imagecreatefromgif($filename);
+         break;
+
+         default:
+             throw new InvalidArgumentException('File "'.$filename.'" is not valid jpg, png or gif image.');
+         break;
+     }
+ }
+
     public function changeBio(Request $request)
     {
 			$user = User::find($request->userid);
